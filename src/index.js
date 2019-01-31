048cae8ae40a264033436af06a094eaeb74057e1
Samuel Stenton <sthstenton@gmail.com>
2019-01-31 09:39:10 +0000

    add resources and have things move on page

--- src/index.js
+++ src/index.js
@@ -1,11 +1,48 @@
 import './style.css';
+import MainLoop from 'mainloop.js';
+import Resource from './resource.js';
 
-function component() {
-  let element = document.createElement('div');
+const gold = new Resource('Gold', 0);
+const happiness = new Resource('Happiness', 1);
+let goldPerSecond = 1;
 
-  element.innerHTML = 'wussssssssssssssssup';
 
-  return element;
+// Settings
+const timePerStep = 500;
+
+/**
+ * Initiate the game
+ */
+function initiate() {
+  // Get details about the users game if they have come back
+
+
+  // Add listeners
+  document.getElementById('city-clicker').addEventListener('click', (el) => {
+    gold.add(1);
+  });
+
+  MainLoop
+    .setUpdate(update)
+    .setDraw(draw)
+    .setSimulationTimestep(timePerStep)
+    .start();
 }
 
-document.body.appendChild(component());
+/**
+ * Update game logic
+ */
+function update() {
+  gold.add((goldPerSecond * happiness.getValue()) / (1000 / timePerStep));
+}
+
+/**
+ * Draw content on page
+ */
+function draw() {
+  document.getElementById('gold-value').innerHTML = Math.floor(gold.getValue());
+  document.getElementById('happiness-value').innerHTML = happiness.getValue();
+}
+
+// Launch the main game loop
+initiate();
---
+++ src/resource.js
@@ -0,0 +1,20 @@
+class Resource {
+  constructor(name, initialValue = 0) {
+    this.name = name;
+    this.value = initialValue;
+  }
+
+  add(amount) {
+    this.value = this.value + amount;
+  }
+
+  remove(amount) {
+    this.value = this.value - amount;
+  }
+
+  getValue() {
+    return this.value;
+  }
+}
+
+export default Resource;
