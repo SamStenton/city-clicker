a29ba5629e7ff0c14d8aa82a1cfd6bc63ef4e3dd
Samuel Stenton <sthstenton@gmail.com>
2019-02-05 16:05:26 +0000

    copy over html to dist

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,9 +1,14 @@
 const path = require('path');
 const dist = path.resolve(__dirname, '../dist');
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
 
 module.exports = {
   entry: path.resolve(__dirname, '../src') + '/index.js',
   mode: 'development',
+  plugins: [
+    new CopyWebpackPlugin([{ from: path.resolve(__dirname, '../src/index.html'), to: dist }])
+  ],
   devServer: {
     contentBase: dist
   }, 
@@ -13,6 +18,16 @@ module.exports = {
   },
     module: {
       rules: [
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+        },
         {
           test: /\.css$/,
           use: [
